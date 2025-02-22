113065b42449a6dad651c347ae6b2afb23ca11a4
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-02-22 23:54:57 +0530

    Responsive Media Query

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ window.addEventListener("scroll", () => {
     header.classList.remove("scrolled"); // Revert to default
   }
 });
+
+
+// Header Button 
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = option.querySelector(".option-text").innerText;
+    sBtn_text.innerText = selectedOption;
+    optionMenu.classList.remove("active");
+  });
+});
